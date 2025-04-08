1f7999361d6d87dfc192262c99f77aef28bf2e4e
VictorScola <victorhugoscola@gmail.com>
2025-04-08 03:48:27 -0300

    Atualização com metodos post, put e delet

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+import random
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
 
 #127.0.0.1:8000/
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
 #127.0.0.1:8000/teste1
 @app.get("/teste1")
 async def funcaoteste():
-    return {"teste": "deu certo"}
+    numero_aleatorio = random.randint(1, 100000)
+    return {
+        "teste": True,
+        "num_aleatorio": numero_aleatorio
+    }
+
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
