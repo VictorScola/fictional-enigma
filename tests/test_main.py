a7f9c6aa48a0fceb3bb10ac49068b146076c02c3
VictorScola <victorhugoscola@gmail.com>
2025-04-28 21:19:49 -0300

    Update test_main.py
    adicionei mais testes para cumprir com o requisitos da atividade.

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,9 +8,38 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 client = TestClient(app)
 
 def test_bola8():
-    response = client.get("/bola8", params={"pergunta": "Vou passar nos testes?"})
+    pergunta = "Vou passar de semestre?"
+    response = client.get("/bola8", params={"pergunta": pergunta})
+    data = response.json()
     assert response.status_code == 200
-    json_data = response.json()
-    assert "pergunta" in json_data
-    assert "resposta" in json_data
-    assert json_data["pergunta"] == "Vou passar no teste?"
+    assert "pergunta" in data
+    assert "resposta" in data
+    assert data["pergunta"] == pergunta
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_teste1():
+    response = client.get("/teste1")
+    data = response.json()
+    assert response.status_code == 200
+    assert "num_aleatorio" in data
+    assert isinstance(data["num_aleatorio"], int)
+    assert data["teste"] is True
+
+def test_cadastro_estudante():
+    estudante = {
+        "name": "Victor",
+        "curso": "ADS",
+        "ativo": True
+    }
+    response = client.post("/estudantes/cadastro", json=estudante)
+    assert response.status_code == 200
+    assert response.json() == estudante
+
+def test_update_estudante_valido():
+    response = client.put("/estudantes/update/1")
+    assert response.status_code == 200
+    assert response.json() is True
